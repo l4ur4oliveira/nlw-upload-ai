20d8028556faf28e9a738690b8ce591740aa3f3f
Laura de Oliveira <l4ur4.88@gmail.com>
2023-09-12 19:16:00 -0300

    Add sidebar with forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Button } from "./components/ui/button";
-import { Github } from "lucide-react";
+import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
 
 export function App() {
     return (
@@ -36,12 +39,82 @@ export function App() {
                         />
                     </div>
                     <p className="text-sm text-muted-foreground">
-                        Lembre-se: você poderá utilizar a variável{" "}
-                        <code className="text-violet-400">{"{transcription}"}</code> no seu prompt para adicionar o
-                        conteúdo de transcrição do vídeo selecionado.
+                        Lembre-se: você poderá utilizar a variável
+                        <code className="text-violet-400 mx-1">{"{transcription}"}</code>
+                        no seu prompt para adicionar o conteúdo de transcrição do vídeo selecionado.
                     </p>
                 </div>
-                <aside className="w-80"></aside>
+
+                <aside className="w-80 space-y-6">
+                    <form className="space-y-6">
+                        <label
+                            htmlFor="video"
+                            className="flex flex-col items-center justify-center gap-2 border border-dashed rounded-md aspect-video cursor-pointer text-sm text-muted-foreground hover:bg-white/5"
+                        >
+                            <FileVideo className="w-4 h-4" />
+                            Selecione um vídeo
+                        </label>
+                        <input type="file" id="video" accept="video/mp4" className="sr-only" />
+
+                        <div className="space-y-2">
+                            <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+                            <Textarea
+                                id="transcription_prompt"
+                                className="h-20 leading-relaxed resize-none"
+                                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+                            />
+                        </div>
+
+                        <Button type="submit" className="w-full">
+                            Carregar vídeo
+                            <Upload className="w-4 h-4 ml-2" />
+                        </Button>
+                    </form>
+
+                    <Separator />
+
+                    <form className="space-y-6">
+                        <div className="space-y-2">
+                            <Label>Prompt</Label>
+                            <Select>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Selecione um prompt..." />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="title">Título para vídeo</SelectItem>
+                                    <SelectItem value="description">Descrição para vídeo</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+                        <div className="space-y-2">
+                            <Label>Modelo</Label>
+                            <Select defaultValue="gpt3.5" disabled>
+                                <SelectTrigger>
+                                    <SelectValue />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            <span className="block text-xs text-muted-foreground italic">
+                                Você poderá customizar esta opção em breve
+                            </span>
+                        </div>
+
+                        <div className="space-y-4">
+                            <Label>Temperatura</Label>
+                            <Slider min={0} max={1} step={0.1} defaultValue={[0.5]} className="cursor-pointer" />
+                            <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                                Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
+                            </span>
+                        </div>
+
+                        <Button type="submit" className="w-full">
+                            Executar <Wand2 className="w-4 h-4 ml-2" />
+                        </Button>
+                    </form>
+                </aside>
             </main>
         </div>
     );
