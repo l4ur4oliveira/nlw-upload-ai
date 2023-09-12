07505a8361a97a47f7a20ff0a09a934287d7b722
Laura de Oliveira <l4ur4.88@gmail.com>
2023-09-12 17:24:23 -0300

    Add main section with the textareas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import { Button } from "./components/ui/button";
+import { Github } from "lucide-react";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
     return (
-        <>
-            <Button>Hellow</Button>
-        </>
+        <div className="min-h-screen flex flex-col">
+            <header className="px-6 py-3 flex items-center justify-between border-b">
+                <h1 className="text-xl font-bold">uplod.ai</h1>
+
+                <div className="flex items-center gap-3">
+                    <span className="text-xs text-muted-foreground">Desenvolvido com 💟 na NLW IA</span>
+
+                    <Separator orientation="vertical" className="h-6" />
+
+                    <Button variant="outline">
+                        <Github className="w-4 h-4 mr-2" />
+                        Github
+                    </Button>
+                </div>
+            </header>
+
+            <main className="flex-1 p-6 flex gap-6">
+                <div className="flex-1 flex flex-col gap-4">
+                    <div className="flex-1 grid grid-rows-2 gap-4">
+                        {/* prettier-ignore */}
+                        <Textarea 
+                            className="resize-none p-4 leading-relaxed" 
+                            placeholder="Inclua o prompt..." 
+                        />
+                        <Textarea
+                            className="resize-none p-4 leading-relaxed"
+                            placeholder="Resultado gerado pela IA..."
+                            readOnly
+                        />
+                    </div>
+                    <p className="text-sm text-muted-foreground">
+                        Lembre-se: você poderá utilizar a variável{" "}
+                        <code className="text-violet-400">{"{transcription}"}</code> no seu prompt para adicionar o
+                        conteúdo de transcrição do vídeo selecionado.
+                    </p>
+                </div>
+                <aside className="w-80"></aside>
+            </main>
+        </div>
     );
 }
